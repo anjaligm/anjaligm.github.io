bd8d9d2ff11e87af39f0f5a70e1d2adb700707a5
anjaligm <vignesh@vigneshs-MacBook-Air.local>
2016-12-11 00:27:29 +0530

    deleted unwanted

---
+++ app/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portfolioApp
+ * @description
+ * # portfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('portfolioApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '../views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: '../views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .when('/app', {
+                templateUrl: '../views/app.html',
+                controller: 'AppsCtrl',
+                controllerAs: 'app'
+            })
+            .when('/contact', {
+                templateUrl: '../views/contact.html',
+                controller: 'ContactCtrl',
+                controllerAs: 'contact'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
